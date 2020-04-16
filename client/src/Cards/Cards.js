e07e569ad16072cb3d69ca658aff1a39f6dcb4b2
rabiamemon <61092918+rabiamemon@users.noreply.github.com>
2020-04-16 19:17:14 +0500

    Updated Navbar

--- client/src/Cards/Cards.js
+++ client/src/Cards/Cards.js
@@ -28,20 +28,23 @@ export class Cards extends Component {
   render() {
     return (
       <div>
-        {this.state.Event.map(Event => (
-          <div Key={Event.id}>
-            <div className="container-fluid d-flex justify-content-">
-              <div className="row">
-                <div className="column">
-                  <Card imgsrc={img1} title={Event.eventname} description={Event.description} />
+        <div className = "container-fluid">
+        <div className = 'row'>
+          {this.state.Event.map(Event => (
+            <div Key = {Event.id}>
+              <div className = "justify-content">
+                <div className = 'col-sm-4'>
+                  <Card imgsrc ={img1} title={Event.eventname} description={Event.description}/>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-    );
-  }
+
+          ))}
+        </div>
+        </div>
+      </div> 
+
+      )  }
 }
 
 export default Cards;
