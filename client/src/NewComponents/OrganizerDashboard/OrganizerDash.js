536373e536dda5996b4a9be144e2f8331f0a0b7e
rabiamemon <61092918+rabiamemon@users.noreply.github.com>
2020-06-01 22:45:04 +0500

    Organizer-Dashboard-Start

---
+++ client/src/NewComponents/OrganizerDashboard/OrganizerDash.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Header from './pageheader'
+import Tab from './tabs'
+
+
+
+
+export default class OrganizerDash extends Component {
+    render() {
+        return (
+            <div>
+              <Header/> 
+              <Tab/>
+                
+            </div>
+        )
+    }
+}
+
+
+
