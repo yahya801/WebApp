757acd5f4139ba6cc7d785e5f5e047682258b164
rabiamemon <61092918+rabiamemon@users.noreply.github.com>
2020-06-12 05:37:21 +0500

    Pagination - Charity Card - Almost Done - Just hooks issue

--- client/src/NewComponents/eventpage/Charitycard.js
+++ client/src/NewComponents/eventpage/Charitycard.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Eventpage from "./eventspage";
 import axios from "axios";
 import Pagination from "./Pagination";
@@ -6,24 +6,28 @@ import Pagination from "./Pagination";
 export class eventcard extends Component {
   constructor(props) {
     super(props);
-    let loading = true; 
-    let count;
-    let paginate;
+    let loading = true;
+
     this.state = {
       loading,
       Event: [],
-      count,
-      paginate,
+      
     };
   }
+
+
+
+  
+
   async componentDidMount() {
   await axios.get(`http://localhost:3000/event/countofevent?category=Charity`)
     .then((res)=>{
-      this.setState({count: res.data.count})
+
+      this.state.count = res.data.count;    
       console.log(res.data.count)
     })
- 
-    axios.get(`http://localhost:3000/event/categorysearch?category=Charity&page=2&limit=1`).then((res) => {
+    
+    axios.get(`http://localhost:3000/event/categorysearch?category=Charity&page=1&limit=5`).then((res) => {
       //    const event = res.data[0];
       //  this.setState({ event});
       this.setState({ Event: res.data.events, loading: false });
@@ -43,14 +47,38 @@ export class eventcard extends Component {
     console.log(index);
   };
   render() {
-    let count = this.setState;
-    let paginate = this.setState;
+
+    const [showPerPage, setshowPerPage] = useState(4);
+
+    const [pagination, setPagination] = useState({
+      start: 0,
+      end: showPerPage
+    });
+
+    const onPaginationChange = (start, end) => {
+      console.log(start, end);
+      setPagination({start:start, end:end});
+
+    };
+
+
+
+    
+
+    
+
+   
+
+    //let paginate = (pageNumber) => ({currentPage: this.setState(pageNumber)});
+   
+    
     return (
       <div>
           <div style={{ paddingLeft: "100px" ,paddingTop: "24px" }}>
+
           <div className="container-fuid">
             <div class="row events-list">
-              {this.state.Event.map((Event) => (
+              {this.state.Event.slice(pagination.start,pagination.end).map((Event) => (
                 <div
                   Key={Event._id}
                   onClick={() => this.singleevent(Event._id)}
@@ -65,17 +93,20 @@ export class eventcard extends Component {
                       vipentry={Event.vipentry}
                     />
                   </div>
-                  <Pagination 
                   
-                  postsPerPage={1}
-                  totalPosts={count}
-                  paginate={paginate}
-                  
-                  />
                 </div>
               ))}
             </div>
           </div>
+          <Pagination
+          showPerPage = {showPerPage}
+          onPaginationChange = {onPaginationChange}
+          total = {Event.length}
+
+
+      
+          
+          />
         </div>
       </div>
     );
--- client/src/NewComponents/eventpage/Pagination.js
+++ client/src/NewComponents/eventpage/Pagination.js
@@ -1,25 +1,75 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { set } from 'mongoose';
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
-  const pageNumbers = [];
+const Pagination = ({showPerPage, onPaginationChange, total}) => {
+    console.log(showPerPage)
+    const [counter, setCounter] = useState(1);
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
-  }
+    useEffect(()=>{
+        console.log("counter changes")
+        const value = showPerPage * counter;
+        console.log("Start value: ", value - showPerPage)
+        console.log("End value: ", value)
+        onPaginationChange(value - showPerPage, value)
+
+    }, [counter]);
+
+    const onButtonClick = (type) => {
+        if (type === "prev"){
+            if (counter === 1){
+                setCounter(1)
+            }
+            else{
+                setCounter(counter-1);
+            }
+        }
+        else if(type==="next"){
+            if(Math.ceil(total/showPerPage) === counter){
+                setCounter(counter)
+            }
+            else{
+                setCounter(counter+1);
+            }
+            
+        }
+    }
+
+
+// const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+//   const pageNumbers = [];
+
+//   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+//     pageNumbers.push(i);
+//   }
 
   return (
-    <nav>
-      <ul className='pagination'>
-        {pageNumbers.map(number => (
-          <li key={number} className='page-item'>
-            <a onClick={() => paginate(number)} href='!#' className='page-link'>
-              {number}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+      <div>
+          <br/>
+          <div className = "container-fluid">
+              <div className = "row">
+                  <div className = "col">
+                      <div className = "d-flex flex-wrap justify-content-between align-self-start">
+                      <button 
+                      className = "btn btn-primary"
+                      onClick = {() => onButtonClick("prev")}
+                      > Previous </button>
+                      <button 
+                      className = "btn btn-primary"
+                      onClick = {() => onButtonClick("next")}
+                      > Next </button>
+
+                      </div>
+
+                  </div>
+
+              </div>
+
+          </div>
+          
+
+      </div>
   );
-};
+        }
+
 
 export default Pagination;
